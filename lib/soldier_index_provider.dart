2f50eccc0afd4012789f881c88d305750e0e82b4
Yamin Satsuki <yamada.osaru@gmail.com>
2023-11-06 21:12:16 +0900

    兵のインデックスのProviderを追加

---
+++ lib/soldier_index_provider.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'soldier_index_provider.g.dart';
+
+@riverpod
+class SoldierIndexNotifier extends _$SoldierIndexNotifier {
+  @override
+  int build() {
+    return 0;
+  }
+}
--- lib/soldier_index_provider.g.dart
+++ lib/soldier_index_provider.g.dart
@@ -1,26 +1,27 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'soldiers_provider.dart';
+part of 'soldier_index_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$soldiersNotifierHash() => r'f6211549f437fa4c4ed85735288182683839cd54';
+String _$soldierIndexNotifierHash() =>
+    r'6e95a725f0f3e5bdbf351e4b7d17beddf0aff437';
 
-/// See also [SoldiersNotifier].
-@ProviderFor(SoldiersNotifier)
-final soldiersNotifierProvider =
-    AutoDisposeNotifierProvider<SoldiersNotifier, List<Soldier>>.internal(
-  SoldiersNotifier.new,
-  name: r'soldiersNotifierProvider',
+/// See also [SoldierIndexNotifier].
+@ProviderFor(SoldierIndexNotifier)
+final soldierIndexNotifierProvider =
+    AutoDisposeNotifierProvider<SoldierIndexNotifier, int>.internal(
+  SoldierIndexNotifier.new,
+  name: r'soldierIndexNotifierProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$soldiersNotifierHash,
+      : _$soldierIndexNotifierHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$SoldiersNotifier = AutoDisposeNotifier<List<Soldier>>;
+typedef _$SoldierIndexNotifier = AutoDisposeNotifier<int>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
