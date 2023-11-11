1e9b5a867181cdab46efdf48b15753f5a102d32e
Yamin Satsuki <yamada.osaru@gmail.com>
2023-11-11 16:06:03 +0900

    兵画面に次と前のボタンを追加

--- lib/ui/pages/status_page.dart
+++ lib/ui/pages/status_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:senjum_simulator/domain/features/soldier_logic.dart';
+import 'package:senjum_simulator/state/soldier_index_provider.dart';
+import 'package:senjum_simulator/state/soldiers_provider.dart';
 import 'package:senjum_simulator/state/stage_lv_provider.dart';
 import 'package:senjum_simulator/state/soldier_provider.dart';
 import 'package:senjum_simulator/ui/widgets/soldier_widget.dart';
@@ -28,6 +30,38 @@ class StatusPage extends ConsumerWidget {
         .updateState(SoldierLogic.determineCapabilities(soldier, stageLv));
   }
 
+  void previousButtonPressed(WidgetRef ref) {
+    int soldierIndex = ref.read(soldierIndexNotifierProvider);
+
+    if (soldierIndex - 1 >= 0) {
+      soldierIndex--;
+    } else {
+      soldierIndex = 29;
+    }
+
+    final soldiers = ref.read(soldiersNotifierProvider);
+    final previousSoldier = soldiers[soldierIndex];
+
+    ref.read(soldierNotifierProvider.notifier).updateState(previousSoldier);
+    ref.read(soldierIndexNotifierProvider.notifier).updateState(soldierIndex);
+  }
+
+  void nextButtonPressed(WidgetRef ref) {
+    int soldierIndex = ref.read(soldierIndexNotifierProvider);
+
+    if (soldierIndex + 1 < 30) {
+      soldierIndex++;
+    } else {
+      soldierIndex = 0;
+    }
+
+    final soldiers = ref.read(soldiersNotifierProvider);
+    final nextSoldier = soldiers[soldierIndex];
+
+    ref.read(soldierNotifierProvider.notifier).updateState(nextSoldier);
+    ref.read(soldierIndexNotifierProvider.notifier).updateState(soldierIndex);
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final soldier = ref.watch(soldierNotifierProvider);
@@ -47,22 +81,49 @@ class StatusPage extends ConsumerWidget {
       child: const Text('初期化'),
     );
 
-    final buttons = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        randomGenerateButton,
-        growButton,
-        initializeButton,
-      ],
+    final buttons = SizedBox(
+      width: 350,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          randomGenerateButton,
+          growButton,
+          initializeButton,
+        ],
+      ),
+    );
+
+    final previousButton = ElevatedButton(
+      onPressed: () => previousButtonPressed(ref),
+      child: const Text('前'),
+    );
+
+    final nextButton = ElevatedButton(
+      onPressed: () => nextButtonPressed(ref),
+      child: const Text('次'),
+    );
+
+    final moveButtons = SizedBox(
+      width: 350,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          previousButton,
+          nextButton,
+        ],
+      ),
     );
 
     return Scaffold(
       appBar: AppBar(),
       body: Container(
+        width: double.infinity,
         color: const Color.fromARGB(255, 40, 40, 40),
         child: Column(
           children: [
             SoldierWidget(soldier: soldier),
+            const SizedBox(height: 10),
+            moveButtons,
             const SizedBox(height: 10),
             buttons,
           ],
